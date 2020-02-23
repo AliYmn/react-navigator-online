3f9ba4475eeb2cde832b65a197896ba0f9413c4f
Ali Yaman <ali.yaman@orthero.com.tr>
2020-02-23 23:19:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class NavigatorOnline extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: navigator.onLine,
+        };
+    }
+
+    getStatus() {
+        this.setState({ online: navigator.onLine })
+        if (this.props.onLine) {
+            this.props.onChange(navigator.online)
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('online', this.getStatus.bind(this));
+        window.addEventListener('offline', this.getStatus.bind(this));
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export default NavigatorOnline;
